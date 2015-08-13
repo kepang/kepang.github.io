7b1b2445c2491568877d0e9582fb485236651735
kp <kepang.zz@gmail.com>
2015-08-13 00:58:29 -0700

    Test disable animations for mobile

--- js/helper.js
+++ js/helper.js
@@ -1,17 +1,26 @@
 $(document).ready( function() {
 
+	// disable skrollr if using handheld device
+	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) == false ) {
+		$("#EA-intro-wrap").attr("data-smooth-scrolling", "off");
+		$("#EA-intro-wrap").attr("data-bottom-top", "opacity:0");
+		$("#EA-intro-wrap").attr("data-center-top", "opacity:1");
+		$("#EA-intro-wrap").attr("data-anchor-target", "#EA-intro-wrap");
+
+		$("#p1").attr("data-40p-start", "opacity:0");
+
+	}
+
+
+
 	skrollr.init({
+		smoothScrolling:false,
 
 		mobileCheck: function() {
 			return false;
 		}
 
 	});
-
-	// disable skrollr if using handheld device
-	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-		$("#EA-intro-wrap").attr("data-smooth-scrolling", "off");
-	}	
 
 	
 	// bind anchor to click
