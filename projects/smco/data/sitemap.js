86682c8624d7902962037d45e0cdfb6ac7454957
kp <kepang.zz@gmail.com>
2015-11-10 12:18:00 -0800

    Removed sm.co files to clean directory

--- projects/smco/data/sitemap.js
+++
@@ -1,8 +0,0 @@
-﻿var sitemap = 
-
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,[_(c,d,e,f,g,h),_(c,i,e,f,g,j),_(c,k,e,f,g,l),_(c,m,e,f,g,n),_(c,o,e,f,g,p,q,[_(c,r,e,f,g,s),_(c,t,e,f,g,u),_(c,v,e,f,g,w),_(c,x,e,f,g,y),_(c,z,e,f,g,A)]),_(c,B,e,f,g,C),_(c,D,e,f,g,E,q,[_(c,F,e,f,g,G)]),_(c,H,e,f,g,I)]);}; 
-var b="rootNodes",c="pageName",d="Home",e="type",f="Wireframe",g="url",h="Home.html",i="02",j="02.html",k="03",l="03.html",m="04",n="04.html",o="A",p="A.html",q="children",r="A1",s="A1.html",t="A2",u="A2.html",v="A3",w="A3.html",x="A4",y="A4.html",z="A5",A="A5.html",B="B",C="B.html",D="C",E="C.html",F="C1",G="C1.html",H="Flow Chart",I="Flow_Chart.html";
-return _creator();
-})();
